3f6846f178edda323a37069666963e60698a3117
konkeydong2019 <51100351+konkeydong2019@users.noreply.github.com>
2020-09-10 21:22:58 +1200

    Add files via upload

--- script.js
+++ script.js
@@ -12,3 +12,33 @@ function ope() {
     z.style.right = "70vw";
   }
 }
+
+
+function ord(n) {
+
+  var drink = ["Short Black", "Macchiato", "Ristretto", "Long Black", "Americano", "Flat White",
+"Cappuccino", "Latte", "Mochaccino", "Mocha Latte", "Hot Chocolate", "Deluxe Hot Chocolate", 
+"Chai Latte", "Iced Coffee", "Iced Chocolate", "Iced Chai"];
+
+  var x = document.getElementById("order-win");
+  var z = document.getElementById("fade");
+
+  if (x.style.opacity === "block"){
+	x.style.display = "none";
+	z.style.display = "none";
+	x.style.opacity = "0";
+	z.style.opacity = "0";
+  } else{
+	x.style.display = "block";
+	z.style.display = "block";
+	x.style.opacity = "100%";
+	z.style.opacity = "50%";
+
+	document.getElementById("zxcv").innerHTML = drink[n];
+	}
+}
+
+function q(){
+document.getElementById('order-win').style.display = 'none';
+document.getElementById('fade').style.display = 'none';
+}
